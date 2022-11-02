470c9528f5aecc10621ed4188b8d17de65cc67e7
Adrià <adria.lop.mar@gmail.com>
2022-11-02 16:11:14 +0100

    main and welcome components

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,20 +1,40 @@
 <template>
-    <ul class="flex">
-  <li class="mr-6">
-    <a class="text-blue-500 hover:text-blue-800" href="#">Active</a>
-  </li>
-  <li class="mr-6">
-    <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
-  </li>
-  <li class="mr-6">
-    <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
-  </li>
-  <li class="mr-6">
-    <a class="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
-  </li>
-</ul>
+  <ul class="flex justify-around">
+    <div>
+      <li class="mr-6">
+        <img
+          src="https://res.cloudinary.com/djqzi4hgo/image/upload/v1667397304/vue%20portfolio/navbar/Firma_j6wfhw.gif"
+          alt=""
+        />
+      </li>
+    </div>
+    <div class="flex justify-around w-4/5">
+
+      <li class="mr-6">
+        <a class="text-blue-500 hover:text-blue-800" href="#">Home</a>
+      </li>
+      <li class="mr-6">
+        <a class="text-blue-500 hover:text-blue-800" href="#">Projects</a>
+      </li>
+      <li class="mr-6">
+        <a class="text-blue-500 hover:text-blue-800" href="#">About me</a>
+      </li>
+    </div>
+    <!-- <li class="mr-6">
+      <a class="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
+    </li> -->
+  </ul>
 </template>
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <style scoped>
+ul {
+  margin-top: 15px;
+box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 20px -20px;
+}
+img {
+  height: 50px;
+}
+li {
+  align-self: center;
+}
 </style>
---
+++ src/components/welcome.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="w-100 main-welcome flex flex-col">
+
+    <h1 class="text-center text-6xl text-white mb-12">Adrià López Martínez</h1>
+    <p class="text-white text-center">Front end developer</p>
+    <p class="text-white text-center">
+      Motivated to build the internet of today to improve the internet of
+      tomorrow we all built yesterday.
+    </p>
+ 
+    <div class="container-buttons flex justify-center">
+
+        <n-button tertiary class="btn mr-5">About me</n-button>
+        <n-button tertiary class="btn">Projects</n-button>
+    </div>
+</div>
+
+</template>
+<script setup lang="ts">
+import { NButton } from 'naive-ui';
+
+</script>
+<style scoped>
+.main-welcome {
+  background-color: rgb(0, 73, 107);
+  height: calc(100vh - 65px);
+  /* flex-direction: column; */
+  justify-content: center;
+  
+}
+.btn{
+   color: white;
+}
+.btn:hover{
+    background-color: aqua;
+}
+</style>
