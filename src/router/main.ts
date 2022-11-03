4606844bc6467e9eb13f3b0fac30366f222a1af0
Adrià <adria.lop.mar@gmail.com>
2022-11-03 20:12:23 +0100

    updated & working on router

--- src/router/main.ts
+++ src/router/main.ts
@@ -1,11 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
-import home from '../views/home.vue'
+import home from "../views/home.vue";
 
-const routes =[
-    {path: "/", component:home}
-]
+const routes = [
+  { path: "/", name: "home", component: home },
+  {
+    path: "/projects",
+    name: "projects",
+    component: () => import("../components/projects.vue"),
+    children: [
+      { path: "/projects/projectName", name: "singleProject", component: ()=> import("../components/singleProject.vue")},
+    //   { path: "/projectName", name: "singleProject", component: ()=> import("../components/singleProject.vue")},
+    ],
+  },
+];
 
-const history = createWebHistory()
-const router = createRouter({history,routes})
+const history = createWebHistory();
+const router = createRouter({ history, routes });
 
-export default router
+export default router;
