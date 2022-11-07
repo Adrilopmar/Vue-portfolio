882cc3fce49c9af2c1f6e41985ca9ef4e93b0427
Adrià <adria.lop.mar@gmail.com>
2022-11-07 12:34:45 +0100

    working on single project page

--- src/components/singleProject.vue
+++ src/components/singleProject.vue
@@ -1,9 +1,23 @@
 <template>
-<h1>Procrastinan't</h1>
-  
+<h1 class="text-3xl">{{project}}</h1>
 </template>
 
-<script setup>
+<script setup lang="ts">
+
+import {onMounted, ref, watch, watchEffect} from 'vue';
+import { useRoute } from 'vue-router';
+import infoProjects from '../assets/infoProjects.json';
+
+const route = useRoute()
+const project = ref(route.params.projectName)
+const conse = ()=>{console.log(project.value)}
+onMounted( ()=> console.log(route.params.projectName))
+
+watch(()=>route.params.projectName,(newValue,oldValue)=>{
+    project.value = route.params.projectName
+    console.log(route.params.projectName)
+})
+
 </script>
 
 <style>
