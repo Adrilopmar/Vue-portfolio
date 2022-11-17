5ef81e14675482b9326216633d3dbb4f8b0a9adf
Adrià <adria.lop.mar@gmail.com>
2022-11-17 01:12:06 +0100

    start working about section

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,11 +1,16 @@
 <template>
-    <h2>About me</h2>
-    <p class="text-white text-center">
+  <div class="bg-about flex p-7">
+    <p class="text-white text-center text-3xl m-auto">
       Motivated to build the internet of tomorrow to improve the internet of
       today we all built yesterday.
     </p>
+  </div>
 </template>
 <script setup lang='ts'>
 </script>
 <style scoped>
+.bg-about{
+  min-height: calc(100vh - 65px);
+  background-color: var(--info-bg);
+}
 </style>
